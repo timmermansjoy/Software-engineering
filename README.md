47cc1d8cf126752198818fe9021f7099067d1811
timmermansjoy <61321383+timmermansjoy@users.noreply.github.com>
2021-09-27 19:10:59 +0200

    Created structure in project

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Software-engineering
+
+[![Build Status](<https://dev.azure.com/11904409/Software%20engineering/_apis/build/status/timmermansjoy.Software-engineering%20(1)?branchName=main>)](https://dev.azure.com/11904409/Software%20engineering/_build/latest?definitionId=2&branchName=main)
+[![made-with-python](https://img.shields.io/badge/Made%20with-Python-1f425f.svg)](https://www.python.org/)
+
+<p align="center">
+  <img width="500px" src="https://images.unsplash.com/photo-1538474705339-e87de81450e8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80">
+</p>
+
+## About
+
+This project is created for the course Software Engineering at PXL, with the pourpose of creating a csv parser that can be used to create groups of students.
+
+## Requirements
+
+To use this project it is required to install the dependencies with
+
+```bash
+pip install -e .
+```
+
+For develoment it is required to run.
+
+```bash
+pip install -e ".[development]"
+```
+
+If that doesnt work use `pip3` this is because you also have python 2 installed
---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# https://godatadriven.com/blog/a-practical-guide-to-using-setup
+import os
+import setuptools
+
+directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(name='CSV_Parser',
+                 version='0.0.1',
+                 description='A csv parsing tool.',
+                 author='Joy Timmermans, Nikki Bruls, Stijn Jacobs, Karel Sajdak, Wiktor Kosinski',
+                 license='MIT',
+                 long_description=long_description,
+                 long_description_content_type='text/markdown',
+                 package_dir={"": "src"},
+                 packages=setuptools.find_packages(where="src"),
+                 platforms=['any'],
+                 classifiers=[
+                     "Programming Language :: Python :: 3",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                 ],
+                 install_requires=['pandas'],
+                 python_requires='>=3.6',
+                 extras_require={
+                     'development': ["pytest", "pytest-xdist", "pytest-cov", "autopep8"],
+                     'docker': ["PyQt5", "pyyaml", "rospkg", "opencv-python"]
+                 },
+                 include_package_data=True)
