b474334b51827de95db4a8fb3fdcb8653e5506e5
StijnJacobsPXL <stijn.jacobsje@gmail.com>
2021-09-27 15:06:36 +0200

    first draaaft

---
+++ Group.py
@@ -0,0 +1,8 @@
+class Group:
+    def __init__(self, list_of_students, group_number):
+        self.list_of_students = list_of_students
+        self.group_number = group_number
+    
+    def return_group_members(self):
+        return self.list_of_students
+    
---
+++ main.py
@@ -0,0 +1,54 @@
+import pandas as pd
+from Group import Group
+from Student import Student
+
+
+def get_students_from_csv(path):
+    student_data = pd.read_csv(path)
+    students = []
+    for index, row in student_data.iterrows():
+        students.append(Student(row.firstname, row.lastname, row.email, row.student_number))
+    return students       
+
+def make_group(student_list, group_size, group_number):
+    if len(student_list) != group_size:
+        print("The list of students you passed is not the correct group size")
+    else:
+        print("this group is the correct size")
+        group = Group(student_list, group_number)
+        for student in student_list:
+            student.add_to_group(group_number)
+        return group
+
+def find_groupless_students(student_list):
+    groupless = []
+    for student in student_list:
+        if not student.group_number:
+            groupless.append(student)
+            
+def print_students(student_list):
+    for student in student_list:
+        print(student + "\n")
+
+def main():
+    file_path = input("Give the path of the file: ")
+    newline = input("Give the newline character: ")
+    delimiter = input("Give the delimiter character: ")
+    quote_character = input("Give the quote character: ")
+    students = get_students_from_csv(file_path, newline, delimiter, quote_character)
+    group_size = input("How many students will this group consist of?")
+    number_of_groups = len(students) / group_size if len(students) % group_size == 0 else (len(students) / group_size) + 1
+    groups = []
+    current_group_index = 1
+    for i in range(number_of_groups):
+        students_in_group = []
+        for j in range(group_size):
+            student_first_name = input("give the first name of student {}".format(j + 1))
+            student_last_name = input("give the last name of student {}".format(j + 1))
+            student_email = input("give the e-mail of student {}".format(j + 1))
+            student_number = input("give the number of student{}".format(j + 1))
+            current_student = Student(student_first_name, student_last_name, student_email, student_number)
+            students_in_group.append(current_student)
+        groups.append(make_group(students_in_group, group_size, current_group_index))
+        current_group_index += 1
+    
