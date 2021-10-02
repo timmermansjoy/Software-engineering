e42c7f8e150cea1ed2692f32739f7e2026fe47d8
StijnJacobsPXL <StijnJacobsPXL@users.noreply.github.com>
2021-10-02 19:11:42 +0000

    autopep8 action fixes

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 app = Flask(__name__)
 
+
 def get_students_from_csv(path):
     student_data = pd.read_csv(path)
     students = []
@@ -11,10 +12,12 @@ def get_students_from_csv(path):
         students.append(Student(row.Number, row.Gender, row.GivenName, row.Surname, row.EmailAddress, row.GUID))
     return students
 
+
 @app.route("/")
 def index():
     title = "Software Engineering"
     return render_template("form.html", title=title)
+
 
 @app.route('/send', methods=['POST'])
 def send():
