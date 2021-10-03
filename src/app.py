f20c2bafbb1bd871625d7bb6cafb39d5360060f5
timmermansjoy <61321383+timmermansjoy@users.noreply.github.com>
2021-10-03 17:10:56 +0200

    fixed imports

--- src/app.py
+++ src/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request
-from Student import Student
+from student import Student
 import pandas as pd
 
 app = Flask(__name__)
+
 
 def get_students_from_csv(path):
     student_data = pd.read_csv(path)
@@ -11,10 +12,12 @@ def get_students_from_csv(path):
         students.append(Student(row.Number, row.Gender, row.GivenName, row.Surname, row.EmailAddress, row.GUID))
     return students
 
+
 @app.route("/")
 def index():
     title = "Software Engineering"
     return render_template("form.html", title=title)
+
 
 @app.route('/send', methods=['POST'])
 def send():
