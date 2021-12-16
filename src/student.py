8c060b96b479e2d315e7ec8a77199751a35e4f74
NikkiBrulsPXL <11900461@student.pxl.be>
2021-12-16 21:52:27 +0100

    changed import of Person file in student.py

--- src/student.py
+++ src/student.py
@@ -1,8 +1,8 @@
-from Person import Person
+import Person
 from Print_student import Print_student
 
 
-class Student(Person):
+class Student(Person.Person):
     def __init__(self, number, gender, given_name, surname, email, GUID, group_number):
         self.number = number
         self.gender = gender
